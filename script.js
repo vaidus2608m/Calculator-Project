f465989fc94b911e6a31cdf59eafa623321164eb
vaidus2608m <vaidus2608m@gmail.com>
2025-12-08 20:34:45 +0530

    ADDING USE OF SINGLE OPERATOR ONLY

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
-const specialChars = ["%", "/", "*", "-", "+", "="];
+const specialChars = ["%", "/", "*", "-", "+"];
+const equalsTo = '=';
 let output = "";
 
 const calculate = (btnValue) => {
-    // display.focus();
     if(btnValue == "AC"){
         output = "";
     } else if(btnValue == "DEL"){
         output = output.toString().slice(0,-1);
-    } else if(output !== "" && btnValue == "="){
+    } else if(output !== "" && btnValue == equalsTo){
         output = eval(output.replace("%", "/100"));
     } else {
         if(output == "" && specialChars.includes(btnValue)) return;
+        let lastChar = output.slice(-1);
+        if(specialChars.includes(lastChar) && specialChars.includes(btnValue)){
+            output = output.slice(0, -1) + btnValue;
+            display.value = output;
+            return;
+        }
         output += btnValue;
     }
     display.value = output;
@@ -21,5 +27,4 @@ const calculate = (btnValue) => {
 
 buttons.forEach((button) => {
     button.addEventListener("click", (res) => calculate(res.target.dataset.value));
-
 });
